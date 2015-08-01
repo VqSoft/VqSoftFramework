200cd473668e6c3c0f3b28c6c6337ec76f1744d9
Eric.Zhao <imap2000@163.com>
2015-08-01 17:14:18 +0800

    Add new project:VqSoft.Framework.Common

---
+++ VqSoft.Framework/VqSoft.Framework.Common/Utils/CodeTimer.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VqSoft.Framework.Common.Utils
+{
+    /// <summary>
+    /// Requirement: NET 4.0+
+    /// Used to test code performance
+    /// Examples: CodeTimer.Time("Thread Sleep", 1, () => { Thread.Sleep(3000); });
+    /// </summary>
+    public static class CodeTimer
+    {
+        /// <summary>
+        /// Should be called before the test
+        /// </summary>
+        public static void Initialize()
+        {
+            //Reduce the OS's dispatch effect
+            Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.High;
+            Thread.CurrentThread.Priority = ThreadPriority.Highest;
+
+            //Pre hot so that JIT can compile IL to native code.
+            Time("", 1, () => { });
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name">Test name ,can not been empty</param>
+        /// <param name="iteration">Code execution times</param>
+        /// <param name="action">Function to be tested</param>
+        public static void Time(string name, int iteration, Action action)
+        {
+            if (String.IsNullOrEmpty(name)) return;
+
+            // 1.
+            ConsoleColor currentForeColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(name);
+
+            // 2.
+            GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced);
+            int[] gcCounts = new int[GC.MaxGeneration + 1];
+            for (int i = 0; i <= GC.MaxGeneration; i++)
+            {
+                gcCounts[i] = GC.CollectionCount(i);
+            }
+
+            // 3.
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            ulong cycleCount = GetCycleCount();
+            for (int i = 0; i < iteration; i++) action();
+            ulong cpuCycles = GetCycleCount() - cycleCount;
+            watch.Stop();
+
+            // 4.
+            Console.ForegroundColor = currentForeColor;
+            Console.WriteLine("\tTime Elapsed:\t" + watch.ElapsedMilliseconds.ToString("N0") + "ms");
+            Console.WriteLine("\tCPU Cycles:\t" + cpuCycles.ToString("N0"));
+
+            // 5.
+            for (int i = 0; i <= GC.MaxGeneration; i++)
+            {
+                int count = GC.CollectionCount(i) - gcCounts[i];
+                Console.WriteLine("\tGen " + i + ": \t\t" + count);
+            }
+
+            Console.WriteLine();
+        }
+
+        private static ulong GetCycleCount()
+        {
+            ulong cycleCount = 0;
+            QueryThreadCycleTime(GetCurrentThread(), ref cycleCount);
+            return cycleCount;
+        }
+
+        /// <summary>
+        /// Only Vista and Windows 2008 support
+        /// </summary>
+        /// <param name="threadHandle"></param>
+        /// <param name="cycleTime"></param>
+        /// <returns></returns>
+        [DllImport("kernel32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool QueryThreadCycleTime(IntPtr threadHandle, ref ulong cycleTime);
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetCurrentThread();
+    }//end of class
+}
---
+++ VqSoft.Framework/VqSoft.Framework.Common/Utils/CodeTimerNet20.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VqSoft.Framework.Common.Utils
+{
+    public interface IAction
+    {
+        void Action();
+    }
+
+    /// <summary>
+    /// For net 2.0
+    /// Example: CodeTimer.Time("Thread Sleep", 10000000, new TestEmptyMethod());
+    /// Notice: TestEmptyMethod must implement IAction
+    /// </summary>
+    public static class CodeTimerNet20
+    {
+        /// <summary>
+        /// The accuracy is less than QueryThreadCycleTime, but it 
+        /// can been used on lower version windows.
+        /// </summary>
+        /// <param name="hThread"></param>
+        /// <param name="lpCreationTime"></param>
+        /// <param name="lpExitTime"></param>
+        /// <param name="lpKernelTime"></param>
+        /// <param name="lpUserTime"></param>
+        /// <returns></returns>
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool GetThreadTimes(IntPtr hThread, out long lpCreationTime,
+          out long lpExitTime, out long lpKernelTime, out long lpUserTime);
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetCurrentThread();
+
+        public delegate void ActionDelegate();
+
+        private static long GetCurrentThreadTimes()
+        {
+            long l;
+            long kernelTime, userTimer;
+            GetThreadTimes(GetCurrentThread(), out l, out l, out kernelTime,
+               out userTimer);
+            return kernelTime + userTimer;
+        }
+
+        static CodeTimerNet20()
+        {
+            Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.High;
+            Thread.CurrentThread.Priority = ThreadPriority.Highest;
+
+        }
+
+        public static void Time(string name, int iteration, ActionDelegate action)
+        {
+            if (String.IsNullOrEmpty(name))
+            {
+                return;
+            }
+
+            if (action == null)
+            {
+                return;
+            }
+
+            //1. Print name
+            ConsoleColor currentForeColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(name);
+
+
+            // 2. Record the latest GC counts
+            //GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced);
+            GC.Collect(GC.MaxGeneration);
+            int[] gcCounts = new int[GC.MaxGeneration + 1];
+            for (int i = 0; i <= GC.MaxGeneration; i++)
+            {
+                gcCounts[i] = GC.CollectionCount(i);
+            }
+
+            // 3. Run action
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            long ticksFst = GetCurrentThreadTimes(); //100 nanosecond one tick
+
+            for (int i = 0; i < iteration; i++) action();
+            long ticks = GetCurrentThreadTimes() - ticksFst;
+            watch.Stop();
+
+            // 4. Print CPU
+            Console.ForegroundColor = currentForeColor;
+            Console.WriteLine("\tTime Elapsed:\t\t" +
+               watch.ElapsedMilliseconds.ToString("N0") + "ms");
+            Console.WriteLine("\tTime Elapsed (one time):" +
+               (watch.ElapsedMilliseconds / iteration).ToString("N0") + "ms");
+
+            Console.WriteLine("\tCPU time:\t\t" + (ticks * 100).ToString("N0")
+               + "ns");
+            Console.WriteLine("\tCPU time (one time):\t" + (ticks * 100 /
+               iteration).ToString("N0") + "ns");
+
+            // 5. Print GC
+            for (int i = 0; i <= GC.MaxGeneration; i++)
+            {
+                int count = GC.CollectionCount(i) - gcCounts[i];
+                Console.WriteLine("\tGen " + i + ": \t\t\t" + count);
+            }
+
+            Console.WriteLine();
+
+        }
+
+        public static void Time(string name, int iteration, IAction action)
+        {
+            if (String.IsNullOrEmpty(name))
+            {
+                return;
+            }
+
+            if (action == null)
+            {
+                return;
+            }
+
+            //1. Print name
+            ConsoleColor currentForeColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(name);
+
+
+            // 2. Record the latest GC counts
+            //GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced);
+            GC.Collect(GC.MaxGeneration);
+            int[] gcCounts = new int[GC.MaxGeneration + 1];
+            for (int i = 0; i <= GC.MaxGeneration; i++)
+            {
+                gcCounts[i] = GC.CollectionCount(i);
+            }
+
+            // 3. Run action
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            long ticksFst = GetCurrentThreadTimes(); //100 nanosecond one tick
+
+            for (int i = 0; i < iteration; i++) action.Action();
+            long ticks = GetCurrentThreadTimes() - ticksFst;
+            watch.Stop();
+
+            // 4. Print CPU
+            Console.ForegroundColor = currentForeColor;
+            Console.WriteLine("\tTime Elapsed:\t\t" +
+               watch.ElapsedMilliseconds.ToString("N0") + "ms");
+            Console.WriteLine("\tTime Elapsed (one time):" +
+               (watch.ElapsedMilliseconds / iteration).ToString("N0") + "ms");
+
+            Console.WriteLine("\tCPU time:\t\t" + (ticks * 100).ToString("N0")
+                + "ns");
+            Console.WriteLine("\tCPU time (one time):\t" + (ticks * 100 /
+                iteration).ToString("N0") + "ns");
+
+            // 5. Print GC
+            for (int i = 0; i <= GC.MaxGeneration; i++)
+            {
+                int count = GC.CollectionCount(i) - gcCounts[i];
+                Console.WriteLine("\tGen " + i + ": \t\t\t" + count);
+            }
+
+            Console.WriteLine();
+
+        }
+    }//end of class
+}
